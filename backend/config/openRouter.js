f9a7ab7babfa276c6827c8111c55f915b54b1514
SARANSH SANTOSHKUMAR MISHRA <saransh.mishra23@spit.ac.in>
2026-08-24 21:46:01 +0530

    Update API endpoint and model in openRouter.js

--- backend/config/openRouter.js
+++ backend/config/openRouter.js
@@ -1,35 +1,33 @@
 export const generateResponse = async (prompt) => {
     const res = await fetch(
-        'https://openrouter.ai/api/v1/chat/completions',
+        "https://openrouter.ai/api/v1/chat/completions",
         {
-            method: 'POST',
+            method: "POST",
             headers: {
                 Authorization: `Bearer ${process.env.OPENROUTER_API_KEY}`,
-                'HTTP-Referer': '<YOUR_SITE_URL>',
-                'X-OpenRouter-Title': '<YOUR_SITE_NAME>',
-                'Content-Type': 'application/json',
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({
-                model: 'deepseek/deepseek-chat',
+                model: "nvidia/nemotron-3-ultra-550b-a55b:free",
                 messages: [
                     {
-                        role: 'system',
-                        content: 'You must return only valid raw json'
+                        role: "system",
+                        content: "You must return only valid raw json"
                     },
                     {
-                        role: 'user',
-                        content: prompt,
-                    },
+                        role: "user",
+                        content: prompt
+                    }
                 ],
                 temperature: 0.2,
                 max_tokens: 12000
-            }),
+            })
         }
     );
 
     if (!res.ok) {
         const err = await res.text();
-        throw new Error("openrouter err" + err);
+        throw new Error("openrouter err " + err);
     }
 
     const data = await res.json();
