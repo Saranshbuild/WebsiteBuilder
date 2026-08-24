70f9bff0b144d98fbb4df1b630b980ca7fb0b549
SARANSH SANTOSHKUMAR MISHRA <saransh.mishra23@spit.ac.in>
2026-08-24 08:54:33 +0530

    Refactor isAuthenticated middleware for token validation

--- backend/middleware/isAuthenticated.js
+++ backend/middleware/isAuthenticated.js
@@ -1,22 +1,28 @@
-import jwt from "jsonwebtoken"
-import { User } from "../models/userModel.js"
+export const isAuthenticated = async (req, res, next) => {
+    try {
 
+        console.log("COOKIE HEADER:", req.headers.cookie)
+        console.log("REQ.COOKIES:", req.cookies)
 
-export const isAuthenticated = async(req,res,next)=>{
-    try{
-     const token = req.cookies.token
-     if(!token){
-        return res.status(400).json({message:"token not found"})
-     }
-     const decoded = jwt.verify(token,process.env.SECRET_KEY)
-     req.user = await User.findById(decoded.id)
-     next()
+        const token = req.cookies.token
+
+        if (!token) {
+            return res.status(400).json({
+                message: "token not found"
+            })
+        }
+
+        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+
+        req.user = await User.findById(decoded.id)
+
+        next()
+
+    } catch (error) {
+        console.log("AUTH ERROR:", error)
+
+        return res.status(500).json({
+            message: "Invalid token"
+        })
     }
-    catch(error){
-        console.error("AUTH ERROR:", error);
-
-    return res.status(500).json({
-        message: error.message
-    })
-}
 }
